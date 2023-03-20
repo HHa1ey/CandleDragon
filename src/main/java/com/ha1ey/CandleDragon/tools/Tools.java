bed4b07e146b485dd942ca1f21a3c5078b24b31d
Ha1ey <haley6@126.com>
2023-03-20 15:09:51 +0800

    1、处理UI输入的URL

---
+++ src/main/java/com/ha1ey/CandleDragon/tools/Tools.java
@@ -0,0 +1,14 @@
+package com.ha1ey.CandleDragon.tools;
+
+public class Tools {
+    //判断URL书写规范自动添加http
+    public static String urlParse(String url){
+        if(!url.contains("http")){
+            url = "http://"+url;
+        }
+        if(url.endsWith("/")){
+            url = url.substring(0, url.length() - 1);
+        }
+        return url;
+    }
+}
